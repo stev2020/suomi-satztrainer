0015770685b2e942150fca1fca24433eb40852f5
stev2020 <136755215+stev2020@users.noreply.github.com>
2026-09-23 16:57:41 +0200

    Avoid duplicate account module instance

--- dist/classrooms.js
+++ dist/classrooms.js
@@ -1,4 +1,4 @@
-import {accountUser,accountRequest} from './auth.js?v=95';
+import {accountUser,accountRequest} from './auth.js?v=96';
 import {GRAMMAR_TOPICS,topicNotes} from './grammar-topics.mjs';
 
 const esc=v=>String(v??'').replace(/[&<>"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c]));
